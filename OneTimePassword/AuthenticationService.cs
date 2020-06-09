486e7bc69562fc2e3b9183bf4c337c15e1e946af
YiFangPan <sber13@gmail.com>
2020-06-09 11:40:10 +0800

    Extract Method

--- OneTimePassword/AuthenticationService.cs
+++ OneTimePassword/AuthenticationService.cs
@@ -10,6 +10,30 @@ namespace OneTimePassword
     {
         public bool IsValid(String account, String password)
         {
+            var passwordFromDb = GetPasswordFromDb(account);
+
+            var otp = GetOtp(account);
+
+            return String.Equals(password, $"{passwordFromDb}{otp}");
+        }
+
+        private static string GetOtp(string account)
+        {
+            var httpClient = new HttpClient() {BaseAddress = new Uri("webapi")};
+
+            var response = httpClient.PostAsJsonAsync("api/otps", account).Result;
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"web api error, account:{account}");
+            }
+
+            var otp = response.Content.ReadAsAsync<String>().Result;
+            return otp;
+        }
+
+        private static string GetPasswordFromDb(string account)
+        {
             var passwordFromDb = String.Empty;
 
             using (var cn = new SqlConnection("connection"))
@@ -22,18 +46,7 @@ namespace OneTimePassword
                     }).FirstOrDefault();
             }
 
-            var httpClient = new HttpClient() { BaseAddress = new Uri("webapi")};
-
-            var response = httpClient.PostAsJsonAsync("api/otps", account).Result;
-            
-            if (!response.IsSuccessStatusCode)
-            {
-                throw new Exception($"web api error, account:{account}");
-            }
-
-            var otp = response.Content.ReadAsAsync<String>().Result;
-            
-            return  String.Equals(password, $"{passwordFromDb}{otp}");
+            return passwordFromDb;
         }
     }
 }
